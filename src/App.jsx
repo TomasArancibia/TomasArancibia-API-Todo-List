5230b19bc543180f061e21cc02d8305018af7665
TomasArancibia <tomasarancibia@uc.cl>
2024-03-26 10:39:41 -0300

    PUT API

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css'
 import Task from './components/Task'
 
 function App() {
-  const [task, setTask] = useState("")
+  const [task, setTask] = useState({})
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    setTasks(
-      fetchingTodo(
+    const fetchData = async () => {
+      const getTasks = await fetchingTodo(
         "https://playground.4geeks.com/apis/fake/todos/user/tomasarancibia",
-        "GET"
-      )
-    )
+        "GET");
+      setTasks(getTasks);
+    }
+    fetchData();
   }, [])
 
   const fetchingTodo = async (url, method, body) => {
@@ -29,57 +30,74 @@ function App() {
       headers: {
         "Content-Type": "application/json",
       }
-    }
+    };
 
-    const finalOption = method === "Get" ? option2 : option1
+    const finalOption = method == "GET" ? option2 : option1
 
     try {
       const responce = await fetch(url, finalOption);
-
       if (!responce.ok) {
+        console.log(responce.text()); // Will try to return the exact result as a stri
         throw new Error("Status error in Fetch")
       }
       const data = await responce.json()
       return data;
     } catch (error) {
-      console.log(`Error ${error}`)
+      console.log(`${error}`)
     }
   }
 
   const handleChange = (e) => {
-    setTask(e.target.value);
+    console.log(task)
+    setTask(
+      {
+        label: e.target.value,
+        done: false
+      })
   }
+
   const handleKeyDown = (e) => {
-    
-      if (e.key === "Enter") {
-        e.preventDefault()
-        if (task != "") {
-        setTasks(prevTasks => [...prevTasks, task]);
-        setTask('')
-      }
-    }
+
+    if (e.key === "Enter") {
+      e.preventDefault()
+      const newTasks = [...tasks, task  ];
+      console.log(newTasks)
+      setTasks(newTasks);
+      fetchingTodo(
+        "https://playground.4geeks.com/apis/fake/todos/user/tomasarancibia",
+        "PUT",
+        newTasks
+      );
+    setTask({ label: "" });
+    } 
   }
 
-  const deleteTask = (index, list) => {
-    list.splice(index, 1);
-    setTasks([...list])
-    return list
-  }
+  const handleClick = () => {
+      setTasks([]);
+      fetchingTodo(
+        "https://playground.4geeks.com/apis/fake/todos/user/tomasarancibia",
+        "PUT",
+        []
+      );
+    } 
 
-  const renderTasks = () => tasks.map((task, index) => {
-    return <Task key={index} id={index} detail={task} tasks={tasks} deleteTask={deleteTask} />
+  const renderTasks = () => tasks?.slice(1).map((task, index) => {
+    console.log(task)
+    return <Task key={index} id={index} detail={task.label} />
   })
   return (
     <>
       <h1 className='title '>To Do List </h1>
       <form >
-        <input maxLength={38} minLength={2} type="text" placeholder='Add a task...' onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} value={task} />
+        <input maxLength={38} minLength={2} type="text" placeholder='Add a task...' onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)}  value={task.label}/>
       </form>
       <div className='postit'>
-        {!tasks.length == 0 ? renderTasks() : <div className='taskDiv'><h3 className='taskDetail'>{"Add your first task"}</h3></div>}
+        {!tasks.length  == 0 ? renderTasks() : <div className='taskDiv'><h3 className='taskDetail'>{"Add your first task"}</h3></div>}
       </div>
       <div className='taskCount'>
-        <p>{tasks.length} Task left</p>
+
+        <p>{tasks.length >= 1 ? tasks.length-1: tasks.length} Task left</p>
+        <button onClick={handleClick}> Clear List </button>
       </div>
       <div className='postitEnd1'></div>
       <div className='postitEnd2'></div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
 const Task = (props) => {
-    const [visible, setVisible] = useState(false);
     return (
     <>
-    <div className="taskDiv" onMouseEnter={() => setVisible(true)} onMouseLeave={() => setVisible(false)} >
+    <div className="taskDiv">
         <h3 className="taskDetail">{props.id + 1}: &nbsp; {props.detail}</h3>
-        {visible ? <button onClick={() =>  props.deleteTask(props.id, props.tasks)} className="display-2 deleteBtn" >X</button> : null}
     </div>
     </>
     )
