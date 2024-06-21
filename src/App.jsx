806e55de708dbd0c554d7d141c748a58c1979a17
TomasArancibia <101353494+TomasArancibia@users.noreply.github.com>
2024-06-21 21:13:54 +0000

    TODOLIST FETCH

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,22 @@ function App() {
   const [task, setTask] = useState({})
   const [tasks, setTasks] = useState([])
   const [username, setUsername] = useState("tomasarancibia")
+  const [user, setUser] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
       const getTasks = await fetchingTodo(
-        `https://playground.4geeks.com/apis/fake/todos/user/${username}`,
-        "GET");
-      setTasks(getTasks);
+        `https://playground.4geeks.com/todo/users/${username}`,
+        "GET"
+      );
+      setTasks(Array.isArray(getTasks["todos"]) ? getTasks["todos"] : []);
     }
     fetchData();
   }, [])
+  
 
   const fetchingTodo = async (url, method, body) => {
+    console.log(url, method)
     const option1 = {
       method: method,
       headers: {
@@ -33,38 +37,51 @@ function App() {
       }
     };
 
-    const finalOption = method == "GET" ? option2 : option1
+    const finalOption = method === "GET" ? option2 : option1;
 
     try {
-      const responce = await fetch(url, finalOption);
-      if (!responce.ok) {
-        console.log(responce.text());
-        throw new Error("Status error in Fetch")
+      const response = await fetch(url, finalOption);
+      if (!response.ok) {
+        if(response.status == 404){
+          fetchingTodo(
+            `https://playground.4geeks.com/todo/users/${username}`,
+            "POST",
+            []
+          );
+        }
+        else
+         {
+          console.log("Hola estoy en el else", response.status)
+          throw new Error("Status error in Fetch")
+        }
       }
-      const data = await responce.json()
+      const data = await response.json();
       return data;
     } catch (error) {
+      return [];
     }
   }
 
   const handleChange = (e) => {
-    setTask(
-      {
-        label: e.target.value,
-        done: false
-      })
+    setTask({
+      label: `${e.target.value}`,
+      is_done: false
+    });
+  }
+
+  const handleChangeUser= (e) => {
+    setUser(`${e.target.value}`);
   }
 
   const handleKeyDown = (e) => {
-
     if (e.key === "Enter") {
-      e.preventDefault()
+      e.preventDefault();
       const newTasks = [...tasks, task];
-      console.log(newTasks)
       setTasks(newTasks);
+      console.log(newTasks)
       fetchingTodo(
-        `https://playground.4geeks.com/apis/fake/todos/user/${username}`,
-        "PUT",
+        `https://playground.4geeks.com/todo/todos/${username}`,
+        "POST",
         newTasks
       );
       setTask({ label: "" });
@@ -73,30 +90,54 @@ function App() {
 
   const handleClick = async () => {
     await fetchingTodo(
-      `https://playground.4geeks.com/apis/fake/todos/user/${username}`,
+      `https://playground.4geeks.com/todo/users/${username}`,
       "DELETE",
       []
     );
     await fetchingTodo(
-      `https://playground.4geeks.com/apis/fake/todos/user/${username}`,
+      `https://playground.4geeks.com/todo/users/${username}`,
       "POST",
       []
     );
     setTasks([]);
   }
+  const ChangeUser = async (e) =>{
+    e.preventDefault()
+    setTasks([]);
+    setUsername(user);  
+      fetchingTodo(`https://playground.4geeks.com/todo/users/${username}`,
+      "GET");
+  }
 
-  const renderTasks = () => tasks?.map((task, index) => {
-    return <Task key={index} id={index} detail={task.label} />
-  })
+  const renderTasks = () => tasks?.map((task, index) => (
+    <Task key={index} id={index} detail={task.label} />
+  ));
+  
   return (
     <>
       <div className='activeUser'><p>User: {username} </p></div>
+      <div className='activeUser'>
+        <form onSubmit={(e) => ChangeUser(e)}>
+          <input type="text" value={user} onChange={(e) => handleChangeUser(e)}  placeholder='Select User...'/>
+          <button type="submit">Change User</button>
+        </form>
+      </div>
       <h1 className='title '>To Do List </h1>
-      <form >
-        <input maxLength={38} minLength={2} type="text" placeholder='Add a task...' onChange={(e) => handleChange(e)} onKeyDown={(e) => handleKeyDown(e)} value={task.label} />
+      <form>
+        <input 
+          maxLength={38} 
+          minLength={2} 
+          type="text" 
+          placeholder='Add a task...' 
+          onChange={handleChange} 
+          onKeyDown={handleKeyDown} 
+          value={task.label || ''} 
+        />
       </form>
       <div className='postit'>
-        {tasks.length == 0 ? <div className='taskDiv'><h3 className='taskDetail'>{"Add your first task"}</h3></div>: renderTasks()}
+        {tasks.length === 0 
+          ? <div className='taskDiv'><h3 className='taskDetail'>{"Add your first task"}</h3></div>
+          : renderTasks()}
       </div>
       <div className='taskCount'>
         <p>{tasks.length} Task left</p>
